56b4cd908eec2305bed1a8f901d39166092954ad
michaelstsnow <michael.st.snow@gmail.com>
2018-11-03 18:40:48 +0000

    automatically backed up by learn

--- lib/my_find.rb
+++ lib/my_find.rb
@@ -1,8 +1,9 @@
 require 'pry'
 
 def my_find(collection)
-  i=0;
-  subset_collection=[];
-  while i<collection.size()
+  i = 0
+  while i < collection.length
+    return collection[i] if yield(collection[i])
+    i = i + 1
   end
 end
